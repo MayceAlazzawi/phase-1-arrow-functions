a8ee32093e376f7543ac818ba4eef52ff6cef4b2
MayceAlazzawi <maynab4@gmail.com>
2022-04-15 17:41:37 +0300

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+// const divide = (a, b) => {
+// //   return  a / b;
+// }
+// divide(2000, 100)
+
+function divide(a, b) {
+  return a / b
+};
+divide(2000, 100)
+
+
+// let square = (a) =>  console.log(a * a )
+
+let square = (x) => { return x * x };
+square(2);
+
+let add = (a, b) => {return a + b};
+add(3,4)
